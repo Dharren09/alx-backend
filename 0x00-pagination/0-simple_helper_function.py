befaec6bb315e2f890551642e88929d84d2adc17
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-15 16:34:22 +0300

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,13 +6,13 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    # check if the arguments are valid
+    """check if the arguments are valid"""
     assert isinstance(page, int) and page > 0
     assert isinstance(page_size, int) and page_size > 0
 
-    # calculate the start index and the end index
+    """calculate the start index and the end index"""
     start_index = (page - 1) * page_size
     end_index = (start_index + page_size)
 
-    # return the tuple of start index and end index
+    """return the tuple of start index and end index"""
     return start_index, end_index
