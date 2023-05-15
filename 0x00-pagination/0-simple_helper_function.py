8449b4573f1fc865138d9e7a28dafca5e7d22711
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-15 16:29:38 +0300

    Pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,16 +2,17 @@
 """
 function takes two int arg and returns a tuple
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    # check if the page number is valid
-    if page < 1:
-        raise ValueError("Page number must be greater than or equal to 1.")
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    # check if the arguments are valid
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
 
     # calculate the start index and the end index
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = (start_index + page_size)
 
     # return the tuple of start index and end index
     return start_index, end_index
