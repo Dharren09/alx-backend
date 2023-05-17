2218e40ee206c10dae748860dd7726680fb815d2
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-17 18:15:58 +0300

    Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,40 +2,32 @@
 """creates a class that inherits and is a caching system"""
 
 from base_caching import BaseCaching
-from functools import lru_cache
 
 
 class MRUCache(BaseCaching):
     """least recently used cache"""
-    """initializes the class"""
-
-    USED = {}
+    """initializes the class instance"""
 
     def __init__(self):
         super().__init__()
+        self.keys = []
 
     def put(self, key, item):
         """adds a new key value pair to the caching system"""
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data.update({key: item})
-            self.USED.update({key: 0})
-            if self.cache_data.__len__() > super().MAX_ITEMS:
-                pop_item = max(self.USED)
-                if self.cache_data.get(pop_item) is not None:
-                    self.USED.pop(pop_item)
-                    self.cache_data.pop(pop_item)
-                print("DISCARD: {}".format(pop_item))
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """gets the value from the caching system"""
-        if key is None or self.cache_data.get(key) is None:
-            return None
-
-        if self.USED.get(key) is not None:
-            self.USED[key] += 1
-        else:
-            self.USED[key] = 1
-
-        return self.cache_data.get(key)
+        if key is not None and key in self.cache_data:
+            self.keys.append(self.keys.pop(self.keys.index(key)))
+            return self.cache_data[key]
+        return None
