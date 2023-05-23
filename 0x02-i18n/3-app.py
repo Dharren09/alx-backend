7cc493dff62369f10ffcad43a0abc817518f48a5
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-23 16:47:39 +0300

    Web Flask

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,7 +4,7 @@ instantiates the babel object in th app the stores it in module-level
 variable"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 app = Flask(__name__)
@@ -32,7 +32,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# babel.init_app(app, locale_selector=get_locale)
+"""babel.init_app(app, locale_selector=get_locale)"""
 
 if __name__ == '__main__':
     app.run()
