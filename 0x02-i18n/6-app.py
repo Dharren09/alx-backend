f33fec092b8c3064bab23c29cdf81185b3e2d81a
Dharren09 <dharrydharrenzerah@gmail.com>
2023-05-23 17:25:47 +0300

    Web Flask

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -49,9 +49,7 @@ def index():
     return render_template('6-index.html')
 
 
-"""@babel.localeselector"""
-
-
+@babel.localeselector
 def get_locale():
     local = request.args.get("locale")
     if local and local in app.config["LANGUAGES"]:
@@ -66,7 +64,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 if __name__ == '__main__':
     app.run()
